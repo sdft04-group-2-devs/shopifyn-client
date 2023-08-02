98a493a8b15b616202226a1dfebc3f681e5a3236
Owallah <mikesonowallah@gmail.com>
2023-08-02 10:59:42 +0300

    imported productlist

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Routes,Route} from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 import LogInPage from './pages/LogInPage';
 import './App.css';
+import ProductList from './components/ProductList/ProductList';
 const App = () => {
   return (
     <Routes>
