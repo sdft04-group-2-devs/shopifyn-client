75b5635eb32546dfbdc4f0e19f4b3b86855ba3eb
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-08-07 09:48:26 +0300

    feat:updated userprofile

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 import LogInPage from './pages/LogInPage';
-import DeliveriesPage from './components/deliveries/deliveriesPage'
+import DeliveriesPage from './components/deliveries/deliveriesPage';
 import './App.css';
 import ProductList from './components/ProductList/ProductList';
+import UserProfile from './components/UserProfile/UserProfile';
 const App = () => {
   return (
     <Routes>
-        <Route exact path="/signup" element={<SignUpPage/>} />
-        <Route exact path="/login" element={<LogInPage/>} />
-        <Route exact path="/products" element={<ProductList/>}/>
-        <Route path='/' element= {<DeliveriesPage />} />
-        {/* ...other routes of your app */}
+      <Route exact path="/signup" element={<SignUpPage />} />
+      <Route exact path="/login" element={<LogInPage />} />
+      <Route exact path="/products" element={<ProductList />} />
+      <Route path="/profile" element={<UserProfile email="user@example.com" />} />
+      <Route path="/" element={<DeliveriesPage />} />
+      {/* ...other routes of your app */}
     </Routes>
   );
 };
